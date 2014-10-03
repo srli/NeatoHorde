10295afd888f3c385ec04cc2b5424c2a65a296ee
srli <sophia.li@students.olin.edu>
2014-10-03 14:44:01 -0400

    testfiles

---
+++ test_listener.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python  
+import roslib
+import rospy
+import math
+import tf
+import geometry_msgs.msg
+import turtlesim.srv
+
+if __name__ == '__main__':
+    rospy.init_node('tf_turtle')
+
+    listener = tf.TransformListener()
+
+    rospy.wait_for_service('spawn')
+    spawner = rospy.ServiceProxy('spawn', turtlesim.srv.Spawn)
+    spawner(4, 2, 0, 'bigbird')
+
+    turtle_vel = rospy.Publisher('bigbird/cmd_vel', geometry_msgs.msg.Twist)
+
+    rate = rospy.Rate(10.0)
+    while not rospy.is_shutdown():
+        try:
+            (trans,rot) = listener.lookupTransform('/oscar', '/bigbird', rospy.Time(0))
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            continue
+
+        angular = 4 * math.atan2(trans[1], trans[0])
+        linear = 0.5 * math.sqrt(trans[0] ** 2 + trans[1] ** 2)
+        cmd = geometry_msgs.msg.Twist()
+        cmd.linear.x = linear
+        cmd.angular.z = angular
+        turtle_vel.publish(cmd)
+
+        rate.sleep()
