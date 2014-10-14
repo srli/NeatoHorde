7c23a556ed877a68efda4008b6d876db1f8721c1
srli <sophia.li@students.olin.edu>
2014-10-13 22:51:02 -0400

    rotation good

--- neato_horde/scripts/run_horde.py
+++ neato_horde/scripts/run_horde.py
@@ -7,6 +7,7 @@ from nav_msgs.msg import Odometry
 from geometry_msgs.msg import PoseStamped, Pose, Twist
 from tf.transformations import euler_from_quaternion
 import math
+import numpy as np
 
 class Robot:
 	"""Neato robot class -- [insert better comment here]"""
@@ -17,7 +18,7 @@ class Robot:
 		self.tf_listener = tf.TransformListener()
 		self.initialized = False
 		self.prev_translation = [0,0,0]
-		self.prev_rotation = [0,0]
+		self.prev_rotation = [0,0,0]
 
 	def update_world_loc(self, msg):
 		"""Returns current robot location [x,y] with respect to world frame"""
@@ -68,11 +69,11 @@ class Robot:
 
 	def scrubinputs(self, trans, rot):
 		translation_x = -0.9 - trans[0]
-		translation_y = trans[1]
-		translation_z = trans[2]
-		rotation_1 = -(180.0/math.pi)*rot[1]
-		rotation_2 = -(180.0/math.pi)*rot[2]
-		print rotation_2
+		#translation_y = trans[1]
+		#translation_z = trans[2]
+		#rotation_1 = -np.degrees(rot[1])
+		rotation_z = np.degrees(rot[2])
+		print "rotation Z", rotation_z
 
 		if self.initialized:
 			if abs(translation_x - self.prev_translation[0]) < 0.3:
@@ -89,27 +90,27 @@ class Robot:
 			self.prev_translation[0] = translation_x
 			# self.prev_translation[1] = translation_y
 			# self.prev_translation[2] = translation_z
-			self.prev_rotation[0] = rotation_1
-			self.prev_rotation[1] = rotation_2
+			#self.prev_rotation[0] = rotation_1
+			self.prev_rotation[2] = rotation_z
 			self.initialized = True
-		self.prev_rotation[1] = rotation_2
+		self.prev_rotation[2] = rotation_z
 
 	def follow(self):
 		"""Sends a cmd_vel to follower robots so they'll follow leader robot"""
 		angular_z = 0
 		twist = Twist()
 		print (0.5*self.prev_translation[0])
-		if self.prev_rotation[1] > 10: #rotation takes priority over translation
+		if abs(self.prev_rotation[2]) > 2: #rotation takes priority over translation
 			print "rotating"
-			self.prev_translation[0] = 0
-			angular_z = self.prev_rotation[1]
+			#self.prev_translation[0] = 0
+			angular_z = self.prev_rotation[2]
 		if abs(1-self.prev_translation[0]) < 0.1: #so we're operating over a range
 			self.prev_translation[0] = 0
 		
 		print "TRANSLATE MOVE: ", 0.5*self.prev_translation[0]
 		print "ANGULAR MOVE: ", 0.2*angular_z
 		twist.linear.x = 0.5*self.prev_translation[0]; twist.linear.y = 0; twist.linear.z = 0 #basically we move on 2 axis, x-linear, z-angular
-		twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0.1*angular_z
+		twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = -0.01*angular_z
 		pub.publish(twist)
 
 
@@ -132,10 +133,21 @@ def listener():
 	rate = rospy.Rate(5.0)
 	while not rospy.is_shutdown():
 		try:
-			#(trans,rot) = listener.lookupTransform('/world', '/follower/base_link', rospy.Time(0))
-			(trans,rot) = listener.lookupTransform('/leader/base_link', '/follower/base_link', rospy.Time(0))
-			angles = euler_from_quaternion(rot)
+			(trans_f,rot_f) = listener.lookupTransform('/world', '/follower/base_link', rospy.Time(0))
+			(trans_l,rot_l) = listener.lookupTransform('/world', '/leader/base_link', rospy.Time(0))
+			#(trans,rot) = listener.lookupTransform('/leader/base_link', '/follower/base_link', rospy.Time(0))
+			angles_f = np.array(euler_from_quaternion(rot_f))
+			angles_l = np.array(euler_from_quaternion(rot_l))
         	
+        	# Casts to numpy array for easier working with
+			trans_f = np.array(trans_f)
+			trans_l = np.array(trans_l)
+
+			trans = trans_f - trans_l
+			angles = angles_f - angles_l
+
+        	# trans = np.subtract(trans_l,trans_f)
+        	# angles = np.subtract(angles_l,angles_f)
 			print('-------------------------------------------------------------')
 			print('TRANSLATION', trans)
 			print 'ROTATION:',[(180.0/math.pi)*i for i in angles]
